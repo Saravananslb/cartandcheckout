938fd0715779ca807fccc28242fb1c367e269572
Saravanan <saravananslb2015@gmail.com>
2022-02-14 20:49:02 +0530

    cart and checkout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import './styles/bootstrap.min.css';
+import './styles/common.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route
+					exact
+					path="/cart"
+					element={
+						<Suspense fallback={<div className="loader"></div>}>
+							<Cart />
+						</Suspense>
+					}
+				/>
+				<Route
+					exact
+					path="/checkout"
+					element={
+						<Suspense fallback={<div className="loader"></div>}>
+							<Checkout />
+						</Suspense>
+					}
+				/>
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/jsonData/cartData.js
@@ -0,0 +1,1 @@
+export const cartData = { orderDetails: {orderId: 142, total: 42000, products: [{price: 42000, name: "hp", quantity: 1, productId: 9}]}}
